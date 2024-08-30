8aed32de4a47ac92314f6528fd9895f1e1ca04d7
Drapala <joao.drapala@gmail.com>
2024-08-30 20:22:11 -0300

    fix: ensure test profile is used for JobController tests via @ActiveProfiles('test') annotation

--- src/test/java/com/drapala/jobrecommendationengine/controller/JobControllerTest/JobControllerTest.java
+++ src/test/java/com/drapala/jobrecommendationengine/controller/JobControllerTest/JobControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(JobController.class)
+@ActiveProfiles("test") // Specify the 'test' profile to use the test configuration
 public class JobControllerTest {
 
     @Autowired
